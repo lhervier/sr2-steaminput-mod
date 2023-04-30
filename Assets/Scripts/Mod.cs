ff2d84576a3adfe8559edc18a11e1b994b49de07
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2023-04-30 23:08:30 +0200

    PRemière version qui semble initialiser Steam

---
+++ Assets/Scripts/Mod.cs
@@ -0,0 +1,49 @@
+namespace Assets.Scripts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ModApi;
+    using ModApi.Common;
+    using ModApi.Mods;
+    using UnityEngine;
+    using Steamworks;
+
+    /// <summary>
+    /// A singleton object representing this mod that is instantiated and initialize when the mod is loaded.
+    /// </summary>
+    public class Mod : ModApi.Mods.GameMod
+    {
+        /// <summary>
+        /// Prevents a default instance of the <see cref="Mod"/> class from being created.
+        /// </summary>
+        private Mod() : base()
+        {
+        }
+
+        /// <summary>
+        /// Gets the singleton instance of the mod object.
+        /// </summary>
+        /// <value>The singleton instance of the mod object.</value>
+        public static Mod Instance { get; } = GetModInstance<Mod>();
+
+        /// <summary>
+        /// Mod initialisation
+        /// </summary>
+        protected override void OnModInitialized() {
+            base.OnModInitialized();
+            
+            Debug.Log("Inbitialising Steam Input Mod");
+
+            Debug.Log("Initializing Steam");
+            bool m_bInitialized = false;
+            try {
+                m_bInitialized = SteamAPI.Init();
+            } catch (Exception e) {
+                Debug.LogError(e);
+            }
+            
+        }
+    }
+}
